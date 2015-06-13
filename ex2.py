af4e8e1b641d13a30aa4aa73438e99fab9c2c47a
Lori McCurry <lmccurry@gsandf.com>
2015-06-13 13:11:07 -0500

    Exercises 2 & 3

---
+++ ex2.py
@@ -0,0 +1,3 @@
+print "I could have to code like this." #and here's a comment
+# print "this won't run"
+print "this will"
---
+++ ex3.py
@@ -0,0 +1,30 @@
+print "I will now count my chickens"
+
+#/ before +
+print "Hens", 25 + 30 / 6
+
+#25 * 3 = 75
+#75 % 4 = 3 (ie: 75/4 = 18 R 3; mod returns 3)
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+#4 % 2 = 0
+#1 - 1/4 = 1 (1/4 goes to floor)
+#1 + 6 = 7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1/4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0/4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
